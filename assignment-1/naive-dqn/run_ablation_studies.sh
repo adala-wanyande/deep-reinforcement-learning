73e76972ace228c05c75522c286b9d589c8a79ea
adala-wanyande <awanyande@gmail.com>
2025-03-14 00:30:14 +0100

    refactor: change folder name

--- assignment-1/naive-dqn/run_ablation_studies.sh
+++ assignment-1/naive-dqn/run_ablation_studies.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Starting all Ablation Studies in Parallel..."
 
 python3 ablation-discount/ablation_discount.py &
 python3 ablation-learning-rate/ablation_learning_rate.py &
 python3 ablation-exploration/ablation_exploration.py &
-python3 ablation-network-size/ablation_network_size.py &
+python3 ablation-nn-size/ablation_nn_size.py &
 
 # Wait for all background processes to finish
 wait
